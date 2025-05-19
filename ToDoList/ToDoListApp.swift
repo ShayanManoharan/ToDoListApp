84127198a2ae43f15996ffe53ca2461efc09fdf1
ShayanManoharan <shayanmanoharan@gmail.com>
2025-05-19 19:04:02 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Shayan Manoharan on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
